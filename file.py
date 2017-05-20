2c3ddd06cec8d3eae119995e8a64d69d9f041f75
Mohanad Aly <capmohanad@hotmail.com>
2017-05-21 01:53:26 +0300

    No error message with user press ctrl + c

--- file.py
+++ file.py
@@ -7,22 +7,24 @@ print "Creating The File"
 config = open(filename, 'a')
 
 
-for i in range(15):
-    first = raw_input("Please Enter Machine name: ")
-    second = raw_input("Please Enter The Hostname Of the Machine Or IP Address: ")
-    third = raw_input("Please Enter The Port Number. Skip with Enter: ")
-    fourth = raw_input("Please Enter The User : ")
-    if first:
-        config.write("Host " + first)
-        config.write("\n")
-    config.write("Hostname " + second)
-    config.write("\n")
-    if third: 
-        config.write("Port " + third)
-        config.write("\n")
-    config.write("User " + fourth)
-    config.write("\n")
-    print "Finish"
-    config.write("\n")
-    config.write("\n")
-config.close()
+try:
+    while True:
+            first = raw_input("Please Enter Machine name: ")
+            second = raw_input("Please Enter The Hostname Of the Machine Or IP Address: ")
+            third = raw_input("Please Enter The Port Number. Skip with Enter: ")
+            fourth = raw_input("Please Enter The User : ")
+            if first:
+                config.write("Host " + first)
+                config.write("\n")
+            config.write("Hostname " + second)
+            config.write("\n")
+            if third: 
+                config.write("Port " + third)
+                config.write("\n")
+            config.write("User " + fourth)
+            config.write("\n")
+            print "Finish"
+            config.write("\n")
+            config.write("\n")
+except KeyboardInterrupt:    
+            config.close()
