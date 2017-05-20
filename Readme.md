b957acbc88bb479c398f8fc75eacae4f80d58934
Mohanad Aly <capmohanad@hotmail.com>
2017-05-20 04:10:30 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# python script ssh
+inserting Host, Hostname, Port, User 
+
+
+### Commands for .ssh/config
+
+```sh
+$ cd .ssh/
+$ nano file.py
+$ python file.py
+```
+
+
+Author: Mohanad Aly
---
+++ file.py
@@ -0,0 +1,28 @@
+from sys import argv
+
+script, filename = argv
+
+
+print "Creating The File"
+config = open(filename, 'a')
+
+
+for i in range(15):
+    first = raw_input("Please Enter Machine name: ")
+    second = raw_input("Please Enter The Hostname Of the Machine Or IP Address: ")
+    third = raw_input("Please Enter The Port Number. Skip with Enter: ")
+    fourth = raw_input("Please Enter The User : ")
+    if first:
+        config.write("Host " + first)
+        config.write("\n")
+    config.write("Hostname " + second)
+    config.write("\n")
+    if third: 
+        config.write("Port " + third)
+        config.write("\n")
+    config.write("User " + fourth)
+    config.write("\n")
+    print "Finish"
+    config.write("\n")
+    config.write("\n")
+config.close()
